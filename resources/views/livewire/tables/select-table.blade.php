bf33e8a5d2667d012fc95ab67fe519d847b9a07a
Emilio Vargas Millan <emiliovm@outlook.es>
2023-08-20 20:57:52 +0200

    refactoring: mejorada estetica select de tablas

--- resources/views/livewire/tables/select-table.blade.php
+++ resources/views/livewire/tables/select-table.blade.php
@@ -1,5 +1,5 @@
 <div class="p-6 space-y-6">
     @foreach($tables as $table)
-        <button data-modal-hide="defaultModal" wire:click="tableSelected({{ $table->id }})" class="w-16 h-16 {{ $table->products->count() > 0 ? "bg-red-400" : "bg-blue-400" }}">{{ $table->id }}</button>
+        <button data-modal-hide="defaultModal" wire:click="tableSelected({{ $table->id }})" class="w-16 h-16 rounded shadow-md text-xl {{ $table->products->count() > 0 ? "bg-red-400" : "bg-blue-400" }}">{{ $table->id }}</button>
     @endforeach
 </div>
